5a2a5d060093a3203900729b10b29d449709fb04
lucianocopa45 <45108640@ifts24.edu.ar>
2025-07-22 21:42:02 -0300

    Primer commit

---
+++ src/main/java/com/example/PasionJuego/PasionJuegoApplication.java
@@ -0,0 +1,13 @@
+package com.example.PasionJuego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasionJuegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasionJuegoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PasionJuego/PasionJuegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PasionJuego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasionJuegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
