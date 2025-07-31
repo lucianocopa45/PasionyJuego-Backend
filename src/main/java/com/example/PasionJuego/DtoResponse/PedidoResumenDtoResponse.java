69c6f5fc86b1ae3350b8bec44be6b279c89ddffe
lucianocopa45 <45108640@ifts24.edu.ar>
2025-07-31 17:14:46 -0300

    Agregue atributos de respuesta para mas detalle

--- src/main/java/com/example/PasionJuego/DtoResponse/PedidoResumenDtoResponse.java
+++ src/main/java/com/example/PasionJuego/DtoResponse/PedidoResumenDtoResponse.java
@@ -1,17 +1,29 @@
 package com.example.PasionJuego.DtoResponse;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductoResumenDto {
+public class PedidoResumenDtoResponse {
 
-    private String nombreProducto;
-    private int cantidad;
+    private Long idPedidos;
+    private Long idUsuario;
+    private String nombreUsuario;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime fechaCreacion;
+    private String estado;
+    //private int cantidadProducto;
+    private double total;
+
+    private List<ProductoResumenDto> productos;
 
 }
