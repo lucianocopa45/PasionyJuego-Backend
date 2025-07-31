80babd4c54bf70b8ef3df2df6c17fa41d95beb64
lucianocopa45 <45108640@ifts24.edu.ar>
2025-07-31 17:10:25 -0300

    Implementacion de jpa en Pedido

--- src/main/java/com/example/PasionJuego/Repository/PedidoRepository.java
+++ src/main/java/com/example/PasionJuego/Repository/PedidoRepository.java
@@ -1,2 +1,9 @@
-package com.example.PasionJuego.Repository;public interface PedidoRepository {
+package com.example.PasionJuego.Repository;
+
+import com.example.PasionJuego.Entity.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 }
