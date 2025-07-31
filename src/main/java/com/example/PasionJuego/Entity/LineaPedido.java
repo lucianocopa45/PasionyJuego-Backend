6207acf10a940ee6f8c286ad5b596682d0fe3817
lucianocopa45 <45108640@ifts24.edu.ar>
2025-07-31 16:54:27 -0300

    Agregue los atributos y las notaciones de lombok

--- src/main/java/com/example/PasionJuego/Entity/LineaPedido.java
+++ src/main/java/com/example/PasionJuego/Entity/LineaPedido.java
@@ -1,7 +1,29 @@
 package com.example.PasionJuego.Entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString (exclude = "producto")
 public class LineaPedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id", nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id_producto")
+    private Producto producto;
+
+    private int cantidad;
+
 }
