d48844a897b73e668f660b3490d862106e11d780
lucianocopa45 <45108640@ifts24.edu.ar>
2025-07-31 17:43:22 -0300

    Implementacion de jpa en Usuario

--- src/main/java/com/example/PasionJuego/Repository/UsuarioRepository.java
+++ src/main/java/com/example/PasionJuego/Repository/UsuarioRepository.java
@@ -1,4 +1,9 @@
 package com.example.PasionJuego.Repository;
 
-public interface UsuarioRepository {
+import com.example.PasionJuego.Entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
