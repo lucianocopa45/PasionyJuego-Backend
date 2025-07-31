6080e2dc9f49b24f40d845f3b556d6a4721e351b
lucianocopa45 <45108640@ifts24.edu.ar>
2025-07-31 17:05:41 -0300

    Agregue los atributos de respuesta

--- src/main/java/com/example/PasionJuego/DtoResponse/PedidoDtoResponse.java
+++ src/main/java/com/example/PasionJuego/DtoResponse/PedidoDtoResponse.java
@@ -1,2 +1,26 @@
-package com.example.PasionJuego.DtoResponse;public class PedidoDtoResponse {
+package com.example.PasionJuego.DtoResponse;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class PedidoDtoResponse {
+    private Long idPedidos;
+    private Long idUsuario;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @JsonFormat (shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updateAt;
+
+    private String estado;
+
+    private String metodoPago;
+
+    private Double total;
 }
